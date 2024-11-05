07cdbb7bbe53b6d56a8c04a80cb617bc71e7ad1b
Sugey Huamani Vega <146673884+Sugeyhv@users.noreply.github.com>
2024-11-05 18:23:38 -0500

    Delete kafka_auth.py

--- kafka_auth.py
+++
@@ -1,5 +0,0 @@
-conf = {'bootstrap.servers': "pkc-n00kk.us-east-1.aws.confluent.cloud:9092",
-        'security.protocol':'SASL_SSL',        
-        'sasl.mechanisms':'PLAIN',
-        'sasl.username' : 'GHFIHLMKJ2WPWE2T',
-        'sasl.password' : 'nI9Yo1Sf0m4cYGnltSvuqu8jLGlCXK9eHCaQcOy72ol+bRsLQ0BOcjFTKI0xl1H9'}
